91e2d29cbad48ae69696b5c241bdc1a404cca85d
Lars Kiesow <lkiesow@uos.de>
2018-07-06 18:03:15 +0200

    Database upgrade tests

---
+++ docs/upgrade/.test.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+set -eu
+cd "$(dirname "$0")"
+
+upgrade_dirs="$(ls -1 | grep '_to_')"
+export versions="$(
+  set -eu
+  echo "$upgrade_dirs" | while read dir; do
+    version=$(echo "$dir" | sed 's/[^0-9.].*$/.0/;s/\([0-9]*\.[0-9]*\)\..*$/\1/')
+    if [ "$(echo "$version >= 2.2" | bc -l)" = 1 ]; then
+      if [ -n "$version" ]; then
+        echo "$version $dir"
+      fi
+    fi
+  done | sort -h)"
+
+tmp22sql="$(mktemp)"
+curl -s -L -o "$tmp22sql" https://raw.githubusercontent.com/opencast/opencast/2.2.0/docs/scripts/ddl/mysql5.sql
+
+echo "# Creating database and applying Opencast 2.2 ddl script"
+
+echo "create database octest;" | mysql -u root
+echo "mysql -u root octest < "$tmp22sql""
+mysql -u root octest < "$tmp22sql"
+rm "$tmp22sql"
+
+echo "# Running upgrade scripts"
+
+echo -e "$versions" | while read line; do
+  dir="$(echo "$line" | cut -d' ' -f2)"
+  echo "mysql -u root octest < $dir/mysql5.sql"
+  mysql -u root octest < $dir/mysql5.sql
+done
